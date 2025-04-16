4a8a0e754c56be6865db4499f375fa0716e2d368
Aleksa404 <aleksantic1@gmail.com>
2025-04-16 18:02:53 +0200

    readme

--- Crypto/src/Pages/RegisterPage.tsx
+++ Crypto/src/Pages/RegisterPage.tsx
@@ -72,48 +72,5 @@ export default function RegisterPage() {
         </form>
       </div>
     </div>
-    // <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-    //   <div className="w-full max-w-md bg-white shadow-xl rounded-xl p-8">
-    //     <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
-    //       Register
-    //     </h1>
-    //     <form onSubmit={handleSubmit}>
-    //       <input
-    //         type="text"
-    //         placeholder="Username"
-    //         value={username}
-    //         onChange={(e) => setUsername(e.target.value)}
-    //         className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-    //       />
-    //       <input
-    //         type="text"
-    //         placeholder="Email"
-    //         value={email}
-    //         onChange={(e) => setEmail(e.target.value)}
-    //         className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-    //       />
-    //       <input
-    //         type="password"
-    //         placeholder="Password"
-    //         value={password}
-    //         onChange={(e) => setPassword(e.target.value)}
-    //         className="w-full px-4 py-2 mb-6 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-    //       />
-    //       <input
-    //         type="password"
-    //         placeholder="Confirm Password"
-    //         value={confirmPassword}
-    //         onChange={(e) => setConfirmPassword(e.target.value)}
-    //         className="w-full px-4 py-2 mb-6 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-    //       />
-    //       <button
-    //         type="submit"
-    //         className="w-full bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-md transition duration-300"
-    //       >
-    //         Register
-    //       </button>
-    //     </form>
-    //   </div>
-    // </div>
   );
 }
