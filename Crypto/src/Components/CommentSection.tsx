ed74d441b62758a0191e6920b6602301309ed779
Aleksa404 <aleksantic1@gmail.com>
2025-06-05 16:57:56 +0200

    comments fix

--- Crypto/src/Components/CommentSection.tsx
+++ Crypto/src/Components/CommentSection.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "@/Context/AuthContext";
 import { formatDistanceToNow } from "date-fns";
 
